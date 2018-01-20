94c120adeec9df6c78669c0ff9a808c1c7795c3e
김종훈 <kim0070@gmail.com>
2018-01-20 11:04:40 +0900

    hibernate version downgrading to 5.1.41.final

--- src/main/java/net/java_school/user/User.java
+++ src/main/java/net/java_school/user/User.java
@@ -2,7 +2,7 @@ package net.java_school.user;
 
 import java.util.List;
 
-import javax.validation.constraints.Email;
+import org.hibernate.validator.constraints.Email;
 import javax.validation.constraints.Size;
 
 public class User {
