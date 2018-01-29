1ef745c8176fc0574d4f718799962457de142a24
kimjonghoon <kim0070@gmail.com>
2018-01-29 16:09:35 +0900

    Spring Security 5.0.1

--- src/main/java/net/java_school/user/User.java
+++ src/main/java/net/java_school/user/User.java
@@ -7,65 +7,74 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.Size;
 
 public class User {
-	@NotNull
-	@Email(message="{email.validation.error}")
-	private String email;
-	
-        @NotNull
-        @Size(min=4, message="{passwd.validation.error}")
-	private String passwd;
-        
-        @NotNull
-	@Size(min=2, message="{fullname.validation.error}")
-	private String name;
-	
-        @NotNull
-        @Size(min=6, message="{mobile.validation.error}")
-	private String mobile;
-        
-	private List<String> authorities;
-	
-	public User() {}
-	
-	public User(String email, String passwd, String name, String mobile, List<String> authorities) {
-		this.email = email;
-		this.passwd = passwd;
-		this.name = name;
-		this.mobile = mobile;
-		this.authorities = authorities;
-	}
 
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPasswd() {
-		return passwd;
-	}
-	public void setPasswd(String passwd) {
-		this.passwd = passwd;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getMobile() {
-		return mobile;
-	}
-	public void setMobile(String mobile) {
-		this.mobile = mobile;
-	}
+    @NotNull
+    @Email(message = "{email.validation.error}")
+    private String email;
 
-	public List<String> getAuthorities() {
-		return authorities;
-	}
+    @NotNull
+    @Size(min = 4, message = "{passwd.validation.error}")
+    private String passwd;
 
-	public void setAuthorities(List<String> authorities) {
-		this.authorities = authorities;
-	}
-	
+    @NotNull
+    @Size(min = 2, message = "{fullname.validation.error}")
+    private String name;
+
+    @NotNull
+    @Size(min = 6, message = "{mobile.validation.error}")
+    private String mobile;
+
+    private List<String> authorities;
+
+    public User() {
+    }
+
+    public User(String email, String passwd, String name, String mobile, List<String> authorities) {
+        this.email = email;
+        this.passwd = passwd;
+        this.name = name;
+        this.mobile = mobile;
+        this.authorities = authorities;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public List<String> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(List<String> authorities) {
+        this.authorities = authorities;
+    }
+
 }
